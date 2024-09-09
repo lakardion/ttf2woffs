b2e031fd97940c2c69f51f4116db7e515856c9a8
Damian <lakardion@protonmail.com>
2024-09-09 14:59:32 -0300

    fix(folder) fix issue where cli crashed on unexisting outDir

--- src/services/woff.ts
+++ src/services/woff.ts
@@ -1,4 +1,4 @@
-import { writeFile } from "fs/promises";
+import { writeFile, access, mkdir } from "fs/promises";
 import ttf2woff from "ttf2woff";
 import ttf2woff2 from "ttf2woff2";
 import { WOFF2_EXTENSION, WOFF_EXTENSION } from "./constants.js";
@@ -8,11 +8,22 @@ type WriteWoffArgument = {
   buffer: Buffer;
   outDir?: string;
 };
-export const writeWoff2 = ({
+const createDirIfNotExists = async ({ outDir }: { outDir: string }) => {
+  const outDirExists = await access(outDir)
+    .then(() => true)
+    .catch(() => false);
+  if (!outDirExists) {
+    await mkdir(outDir);
+  }
+};
+export const writeWoff2 = async ({
   destinationFileName,
   buffer,
-  outDir = "./test-fonts",
+  outDir = "./results",
 }: WriteWoffArgument) => {
+  createDirIfNotExists({
+    outDir,
+  });
   const woff2FileBuffer = ttf2woff2(buffer);
   const woff2FilePromise = writeFile(
     `${outDir}/${destinationFileName}.${WOFF2_EXTENSION}`,
@@ -24,8 +35,11 @@ export const writeWoff2 = ({
 export const writeWoff = ({
   destinationFileName,
   buffer,
-  outDir = "./test-fonts",
+  outDir = "./results",
 }: WriteWoffArgument) => {
+  createDirIfNotExists({
+    outDir,
+  });
   const woffFileBuffer = ttf2woff(buffer);
   const woffFilePromise = writeFile(
     `${outDir}/${destinationFileName}.${WOFF_EXTENSION}`,
